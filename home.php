4ee2220a520633e688acd6dea515b592b1722d2a
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-11-19 14:59:07 -0700

    finished home

--- home.php
+++ home.php
@@ -2,12 +2,13 @@
 require_once 'header.php';
 
 echo <<<_Home
-
 <title>Cine-Files</title>
 <head>
 
-    <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
-    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
+<link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+
+
     <style>
         * {
             box-sizing: border-box;
@@ -16,7 +17,7 @@ echo <<<_Home
             width: 100%;
             height: 100%;
         }
-        .wrapper {
+        .wrapper2 {
             height: 100vh;
             width: 150vh;
             margin: 0 auto;
@@ -32,17 +33,6 @@ echo <<<_Home
             height: 100%;
             overflow: hidden;
         }
-        /*.stage_floor {*/
-        /*    content: '';*/
-        /*    position: absolute;*/
-        /*    z-index: 1;*/
-        /*    top: 10%;*/
-        /*    left: 0;*/
-        /*    right: 0;*/
-        /*    height: 150%;*/
-        /*    background-image: radial-gradient(rgba(54,90,143,0.9), rgba(54,90,143,0));*/
-        /*    transform: scale(1.0,0.3);*/
-        /*}*/
         .stage_highlight {
             position: absolute;
             z-index: 1;
@@ -117,73 +107,58 @@ echo <<<_Home
                 transform: translateX(-25vh);
             }
         }
-        #content {
-            background-color: #4E1003;
-            height: 100%;
-            background-position: center;
-            position: relative;
-
-        }
 
 
-        h1 {
-            text-align: center;
-            margin-top: 45px;
-            color: white;
-            font-family: 'Yanone Kaffeesatz', sans-serif;
 
-        }
-        h2 {
-            text-align: center;
-            font-size: 1.5em;
-            font-family: 'Yanone Kaffeesatz', sans-serif;
-        }
-        body{
-            background-image: url("curtain02.jpg");
-            background-size: 100%;
-            background-attachment: fixed;
-            display:grid;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            position:relative;
-        }
-        movie{
-            position: absolute;
-            z-index: 1000 !important;
-            border: #4E1003;
-        }
-        .container{
-            display:inline-block;
-            margin-top: 200px;
-            width: 50rem;
-            height: 40rem;
-            /*box-shadow: 0 0 1rem 0 rbga(0,0,0,.2);*/
-            content: "";
-            position: relative;
-            z-index: 1000 !important;
-            background:inherit;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom:0;
-            box-shadow: inset 0 0 2000px rgba(255,255,255,.5);
-            filter:blur(0px);
-        }
-        #test{
-            font-weight: bold;
-            font-size: xx-large;
+  #content {
+	background-color: #4E1003;
+  	height: 100%;
+	/*background-position: center;*/
+  	background-size: 100%;
+	/*position: relative;*/	
+	background-image: url("curtain02.jpg");		
+ }
+	
 
-        }
+  h1 {
+	text-align: center;
+	margin-top: 45px;
+	color: white;
+	font-family: 'Yanone Kaffeesatz', sans-serif;	
+	
+}
+  h2 {
+	text-align: center;
+	font-size: 1.5em;
+	font-family: 'Yanone Kaffeesatz', sans-serif;
+}
+body{
+	background-color: black;
+}
 
-    </style>
+.container{
+    display:inline-block;
+    width: 50rem;
+    box-shadow: 0 0 1rem 0 rbga(0,0,0,.2);
+    position: relative;
+    box-shadow: inset 0 0 2000px rgba(255,255,255,.5);
+    filter:blur(0px);
+}
+    #test{
+        font-weight: bold;
+        font-size: xx-large;
+
+}
+
+
+
+</style>
 </head>
 
 
 <body>
 <h1>Toto, I've a feeling we're not in Kansas anymore <h1>
-
-    <div class="wrapper">
+    <div class="wrapper2">
 
         <div class="backdrop"></div>
 
@@ -199,23 +174,22 @@ echo <<<_Home
 
         </div>
 
-<div id="movie" style=" position: relative; z-index: 1000">
-    <iframe  width="956" height="538" src="https://www.youtube.com/embed/4HvE_bz9imA" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe style="position:relative; z-index:1000;" width="956" height="538" src="https://www.youtube.com/embed/4HvE_bz9imA" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
-
-<div class="container" style="position: relative; z-index: 1000">
-    <div id="test">
-        <h1 style="color:white; text-shadow: 10px 10px black;">What, Why, and How</h1>
-        <p style="color:white; text-shadow: 7px 7px black;"> Our initial and main goal was to establish a place where movie and show lovers can catalog and curate lists of their favorite cinematic adventures. Within each
-        page on the Cine-Files you will find your personalized list that curate to your movie and show viewing taste! With the ability to add a show or a movie to your list
-        you can select to watch now or save it for later! We have also integrated a wonderful search feature that allows each user to look up their favorite shows and movies
-        with ease!</p>
+    <div class="container" style="position: relative">
+        <div id="test">
+            <h3 style="color:white; text-shadow: 10px 10px black;">What, Why, and How</h3>
+            <p style="color:white; text-shadow: 7px 7px black;"> Our initial and main goal was to establish a place where movie and show lovers can catalog and curate lists of their favorite cinematic adventures. Within each
+            page on the Cine-Files you will find your personalized list that curate to your movie and show viewing taste! With the ability to add a show or a movie to your list
+            you can select to watch now or save it for later! We have also integrated a wonderful search feature that allows each user to look up their favorite shows and movies
+            with ease!</p>
+        </div>
+    
     </div>
 
-</div>
-
-
 </body>
+
+
 
 _Home;
 
