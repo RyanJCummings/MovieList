a8fa8558f03eb8fb762f75cfa21b681c49b502e6
Ryan Cummings <rcummings98@gmail.com>
2020-11-19 15:29:49 -0700

    kjshdf

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $dbhost  = 'localhost';
 
-$dbname  = 'db34b';   // Modify these...
+$dbname  = 'db34';   // Modify these...
 $dbuser  = 'user34';   // ...variables according
 $dbpass  = '34fill';   // ...to your installation
 
