a5afe8148fe244d9d4dbd52ca7a818511cf861cd
Jason-Bengtson <mrfurry43@yahoo.com>
2020-11-20 14:59:52 -0700

    a few fixes

--- header.php
+++ header.php
@@ -118,10 +118,15 @@ _LOGGEDIN;
 echo <<<_GUEST
 
             <div class="navbar">
-                <<a href='index.php'>Home</a>
+                <a href='index.php'>Home</a>
                 <a href='signup.php'>Sign Up</a>
-                <a href='login.php'>Log In</a>
-            </div>
+		<a href='login.php'>Log In</a>
+	    </div>
+  <div style="float:right">
+    <a class="about" href='about.php'style="box-shadow:1px 1px 1px 1px black inset">About</a>
+  </div>
+</div>
+
 _GUEST;
  }
 
